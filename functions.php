50552129b71368cb69ca8a5d03d7bbaf82a05a04
Nico Baguio <nicobaguio41@gmail.com>
2018-04-30 22:06:49 +0800

    Added flickity resources.

--- functions.php
+++ functions.php
@@ -4,6 +4,7 @@ function my_scripts() {
 ;	wp_enqueue_script( 'popper-js', 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js', array('jquery-min-js'), $in_footer=true);
 	wp_enqueue_script( 'bootstrap4-js', 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js', array('popper-js'), $in_footer=true);
 	wp_enqueue_script( 'typed-js', 'https://cdnjs.cloudflare.com/ajax/libs/typed.js/2.0.6/typed.min.js', $in_footer=true);
+	wp_enqueue_script( 'flickity-js', 'https://unpkg.com/flickity@2.1.1/dist/flickity.pkgd.min.js', $in_footer=true);
 	wp_enqueue_script( 'custom-js', get_template_directory_uri() . '/js/custom.js', $in_footer=true);
 };
 
@@ -12,6 +13,7 @@ add_action( 'wp_head', 'my_scripts');
 function my_styles() {
 	wp_enqueue_style( 'style', get_stylesheet_uri());
 	wp_enqueue_style( 'bootstrap-css', 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css');
+	wp_enqueue_style( 'flickity-css', 'https://unpkg.com/flickity@2.1.1/dist/flickity.min.css');
 };
 
 add_action( 'wp_head', 'my_styles');
