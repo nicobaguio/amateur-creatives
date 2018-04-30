b058ee30049f4a958bf2e42fd4307b0eadaa16ed
Nico Baguio <nicobaguio41@gmail.com>
2018-05-01 03:15:29 +0800

    Dynamic header in index.php using WP plugin.

--- functions.php
+++ functions.php
@@ -20,4 +20,5 @@ add_action( 'wp_head', 'my_styles');
 
 add_theme_support( 'post-thumbnails' );
 add_post_type_support( 'page', 'excerpt' );
+add_theme_support( 'custom-header' );
 ?>
--- header.php
+++ header.php
@@ -10,6 +10,6 @@
 	<body id="blogpost">
 		<div class="container-fluid">
 			<header class="row header-container">
-				<div class="row-fluid banner"></div>
+				<div class="row-fluid banner" style="background-image: url('<?php echo get_header_image(); ?>')"></div>
 				<img src="<?php bloginfo('template_directory')?>/images/homepage_circle-logo-small.png" class="header-logo">
 			</header>
