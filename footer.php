4861282a7563bf162e5cf5a4e5ba8cac0dba4bbc
Nico Baguio <nicobaguio41@gmail.com>
2018-05-02 01:28:33 +0800

    Added final footer with proper styling and dynamic year for copy rights.

--- footer.php
+++ footer.php
@@ -1,3 +1,10 @@
+<footer class="container-fluid">
+	<div class="row">
+	</div>
+	<div class="row final-footer">
+		<p class="text-center">&copy; AdobeXD Kit <?php echo date(Y); ?>.<br>Designed by Crafted by Pam. Developed by Nico Baguio.</p>
+	</div>	
+</footer>
 <?php wp_footer(); ?>
 </body>
 </html>
