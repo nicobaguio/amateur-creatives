c259b17e562443f2a1293e3673f67e1a78f2ccad
Nico Baguio <nicobaguio41@gmail.com>
2018-05-03 02:13:24 +0800

    Added links pre-footer with stylings based on high fidelity wireframe.

--- footer.php
+++ footer.php
@@ -1,5 +1,24 @@
 <footer class="container-fluid">
-	<div class="row">
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+		<p class="navbar-brand d-block d-sm-none">Go to...</p>
+		<button class="navbar-toggler align-center" type="button" data-toggle="collapse" data-target="#navlinks" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		<i class="fas fa-chevron-circle-down"></i>
+		</button>
+		<div class="collapse navbar-collapse" id="navlinks">
+			<div class="navbar-nav pre-footer-nav">
+				<a class="nav-item nav-link" href="#"><p class="footer-link">Links</p></a>
+				<a class="nav-item nav-link" href="#"><p class="footer-link">Links</p></a>
+				<a class="nav-item nav-link" href="#"><p class="footer-link">Links</p></a>
+				<a class="nav-item nav-link" href="#"><p class="footer-link">Links</p></a>
+				<a class="nav-item nav-link" href="#"><p class="footer-link">Links</p></a>
+				<a class="nav-item nav-link" href="#"><p class="footer-link">Links</p></a>
+				<a class="nav-item nav-link" href="#"><p class="footer-link">Links</p></a>
+				<a class="nav-item nav-link" href="#"><p class="footer-link">Links</p></a>
+			</div>
+		</div>
+	</nav>
+	<div class="row pre-footer">
+		<p>THIS IS THE PRE FOOTER</p>
 	</div>
 	<div class="row final-footer">
 		<p class="text-center">&copy; AdobeXD Kit <?php echo date(Y); ?>.<br>Designed by Crafted by Pam. Developed by Nico Baguio.</p>
