520a9c03149dd948892e49ebcc62508e0a4276ec
Nico Baguio <nicobaguio41@gmail.com>
2018-05-17 19:35:29 +0800

    Added blog content and minor responsive web design stuff.

--- single.php
+++ single.php
@@ -1,3 +1,19 @@
 <?php get_header(); ?>
-
+<div class="row blog-container">
+<div class="col-lg-2 clearfix">
+</div>
+<div class="col-lg-8 col-12 justify-content-center blog-content">
+		<div class="row blog-title justify-content-center">
+			<h1 class="title mx-5"><?php the_title(); ?></h1>
+		</div>
+		<div class="row d-flex justify-content-center metadata">
+			<p><a href="#">Category</a></p>
+		</div>
+		<div class="col blog-text">
+			<?php the_content(); ?>
+		</div>
+</div>
+<div class="col-lg-2 clearfix">
+</div>
+</div>
 <?php get_footer(); ?>
