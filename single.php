5a7c343424e5408e1116d82cc2963265a3cee106
Nico Baguio <nicobaguio41@gmail.com>
2018-05-23 19:19:41 +0800

    Converted blog-title in single.php to id from class.

--- single.php
+++ single.php
@@ -6,7 +6,7 @@
 		<div class="row d-flex justify-content-center metadata">
 			<p><a href="#">Category</a></p>
 		</div>
-		<div class="row blog-title justify-content-center">
+		<div class="row justify-content-center" id="blog-title">
 			<h1 class="title mx-5"><?php the_title(); ?></h1>
 		</div>
 		<div class="col" id="blog-text">
