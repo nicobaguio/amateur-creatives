0135f5b55643f2f20a75290de3832effb9103600
Nico Baguio <nicobaguio41@gmail.com>
2018-05-23 19:17:35 +0800

    converted blog-text in single.php to id from class.

--- single.php
+++ single.php
@@ -3,13 +3,13 @@
 <div class="col-lg-2 clearfix">
 </div>
 <div class="col-lg-8 col-12 justify-content-center blog-content">
-		<div class="row blog-title justify-content-center">
-			<h1 class="title mx-5"><?php the_title(); ?></h1>
-		</div>
 		<div class="row d-flex justify-content-center metadata">
 			<p><a href="#">Category</a></p>
 		</div>
-		<div class="col blog-text">
+		<div class="row blog-title justify-content-center">
+			<h1 class="title mx-5"><?php the_title(); ?></h1>
+		</div>
+		<div class="col" id="blog-text">
 			<?php the_content(); ?>
 		</div>
 </div>
