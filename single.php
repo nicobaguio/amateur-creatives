8e6a269edcae5d8b3c38e84e65e58154257b790e
Nico Baguio <nicobaguio41@gmail.com>
2018-05-25 11:25:04 +0800

    Fixed spacing in single.php

--- single.php
+++ single.php
@@ -1,19 +1,19 @@
 <?php get_header(); ?>
 <div class="row blog-container">
-<div class="col-lg-1 clearfix">
-</div>
-<div class="col-lg-10 col-12 justify-content-center" id="blog-content">
-		<div class="row d-flex justify-content-center metadata">
-			<p><a href="#">Category</a></p>
-		</div>
-		<div class="row justify-content-center" id="blog-title">
-			<h1 class="title mx-5"><?php the_title(); ?></h1>
-		</div>
-		<div class="col" id="blog-text">
-			<?php the_content(); ?>
-		</div>
-</div>
-<div class="col-lg-1 clearfix">
-</div>
+	<div class="col-lg-1 clearfix">
+	</div>
+	<div class="col-lg-10 col-12 justify-content-center" id="blog-content">
+			<div class="row d-flex justify-content-center metadata">
+				<p><a href="#">Category</a></p>
+			</div>
+			<div class="row justify-content-center" id="blog-title">
+				<h1 class="title mx-5"><?php the_title(); ?></h1>
+			</div>
+			<div class="col" id="blog-text">
+				<?php the_content(); ?>
+			</div>
+	</div>
+	<div class="col-lg-1 clearfix">
+	</div>
 </div>
 <?php get_footer(); ?>
